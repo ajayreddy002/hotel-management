37e52157c182fb52e272af11c2113b7bedbd54b7
manju-405 <manjunath@flujo.io>
2019-10-01 10:17:51 +0530

    my profile

---
+++ src/app/components/my-profile/my-profile.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.scss']
+})
+export class MyProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  myprofile = [
+    {
+      header: "User Name",
+    },
+    {
+      header: "School Name",
+    },
+    {
+      header: "Email",
+    },
+    {
+      header: "Password",
+    },
+    {
+      header: "Address",
+    },
+  ];
+
+}
---
+++ src/app/components/my-profile/my-profile.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyProfileRoutingModule } from './my-profile-routing.module';
+import { MyProfileComponent } from './my-profile.component';
+
+@NgModule({
+  declarations: [
+    MyProfileComponent,
+  ],
+  imports: [
+    CommonModule,
+    MyProfileRoutingModule
+  ]
+})
+export class MyProfileModule { }
